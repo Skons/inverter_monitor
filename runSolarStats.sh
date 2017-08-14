7d1a2dcee9b2651874fe9a6bb9e82400c97ca7fb
Lucas Costi <lucascosti@gmail.com>
2017-08-14 19:21:38 +1000

    Fix date format on daily output log for proper sorting

--- runSolarStats.sh
+++ runSolarStats.sh
@@ -1,2 +1,2 @@
 /usr/bin/perl /home/pi/inverter_monitor/inverter.pl /dev/ttyUSB0 > /home/pi/inverter_monitor/logs/current.txt
-cat /home/pi/inverter_monitor/logs/current.txt >> /home/pi/inverter_monitor/logs/`date +%d%m%Y`-output.txt
+cat /home/pi/inverter_monitor/logs/current.txt >> /home/pi/inverter_monitor/logs/`date +%Y%m%d`-output.txt
